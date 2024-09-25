b968823d936cb96e2192fe8b436aae64160f4af6
yuwzhang <yuwzhang@cisco.com>
2024-09-25 10:05:01 +0800

    feat(gradio-frontend): 1. change frontend theme 2. add default value for dropdown display 3. fixed character set error

--- src/gradio_server.py
+++ src/gradio_server.py
@@ -14,6 +14,7 @@ llm = LLM()
 report_generator = ReportGenerator(llm)
 subscription_manager = SubscriptionManager(config.subscriptions_file)
 
+
 def export_progress_by_date_range(repo, days):
     # 定义一个函数，用于导出和生成指定时间范围内项目的进展报告
     raw_file_path = github_client.export_progress_by_date_range(repo, days)  # 导出原始数据文件路径
@@ -21,18 +22,23 @@ def export_progress_by_date_range(repo, days):
 
     return report, report_file_path  # 返回报告内容和报告文件路径
 
+
+subscriptions = subscription_manager.list_subscriptions()  # 获取已订阅的GitHub项目列表
+
 # 创建Gradio界面
 demo = gr.Interface(
     fn=export_progress_by_date_range,  # 指定界面调用的函数
     title="GitHubSentinel",  # 设置界面标题
     inputs=[
         gr.Dropdown(
-            subscription_manager.list_subscriptions(), label="订阅列表", info="已订阅GitHub项目"
+            subscriptions, label="订阅列表", info="已订阅GitHub项目",
+            value=subscriptions[0] if subscriptions else None
         ),  # 下拉菜单选择订阅的GitHub项目
         gr.Slider(value=2, minimum=1, maximum=7, step=1, label="报告周期", info="生成项目过去一段时间进展，单位：天"),
         # 滑动条选择报告的时间范围
     ],
     outputs=[gr.Markdown(), gr.File(label="下载报告")],  # 输出格式：Markdown文本和文件下载
+    theme=gr.themes.Monochrome()
 )
 
 if __name__ == "__main__":
